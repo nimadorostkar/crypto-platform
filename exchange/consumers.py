68f87a90ccc0662160a6dc865ea04df541726493
Fan Wang <nimadorostkar97@gmail.com>
2022-11-02 16:17:42 +0330

    ExchangeConsumer disconnect

--- exchange/consumers.py
+++ exchange/consumers.py
@@ -7,5 +7,9 @@ class ExchangeConsumer(AsyncJsonWebsocketConsumer):
         await self.accept()
 
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
+        print(text_data)
         if text_data == 'PING':
             await self.send('PONG')
+
+    async def disconnect(self, close_code):
+        self.event_close = True
