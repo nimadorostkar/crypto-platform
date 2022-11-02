1182c5e1ce5fc8aa2482173122a410a385cfef72
Fan Wang <nimadorostkar97@gmail.com>
2022-11-02 17:54:08 +0330

    push

--- core/settings.py
+++ core/settings.py
@@ -89,18 +89,14 @@ ASGI_APPLICATION = 'core.asgi.application'
 
 
 
-
-'''
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
-'''
-
 
 
 
--- exchange/views.py
+++ exchange/views.py
@@ -10,9 +10,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
+
+from time import sleep
+# Import your desired markets from pybit
 from pybit import inverse_perpetual
-
-
+from pybit import spot
 
 
 
@@ -70,41 +72,64 @@ class ExchangeData(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
+
         exchange = models.Exchange.objects.get(id=self.kwargs["id"])
+        api_key=exchange.api_key
+        api_secret=exchange.api_secret
 
-        session = inverse_perpetual.HTTP(endpoint='https://api.bybit.com', api_key=exchange.api_key, api_secret=exchange.api_secret)
-        ws = inverse_perpetual.WebSocket(test=False, api_key=exchange.api_key, api_secret=exchange.api_secret)
+        """
+        An alternative way to import:
+        from pybit.inverse_perpetual import WebSocket, HTTP
+        """
 
-        # Get orderbook.
-        session.orderbook(symbol='BTCUSD')
+        # Set up logging (optional)
+        import logging
+        logging.basicConfig(filename="pybit.log", level=logging.DEBUG,format="%(asctime)s %(levelname)s %(message)s")
 
-        # Create five long orders.
-        orders = [{
-            "symbol": "BTCUSD",
-            "order_type": "Limit",
-            "side": "Buy",
-            "qty": 100,
-            "price": i,
-            "time_in_force": "GoodTillCancel"
-        } for i in [5000, 5500, 6000, 6500, 7000]]
 
-        # Submit the orders in bulk.
-        session.place_active_order_bulk(orders)
+        # Connect with authentication!
+        ws_inverse = inverse_perpetual.WebSocket(
+            test=True,
+            api_key=api_key,  # omit the api_key & secret to connect w/o authentication
+            api_secret=api_secret,
+            # to pass a custom domain in case of connectivity problems, you can use:
+            domain="bytick"  # the default is "bybit"
+        )
 
-        # Check on your order and position through WebSocket.
+        # Let's fetch the orderbook for BTCUSD. First, we'll define a function.
         def handle_orderbook(message):
+            # I will be called every time there is new orderbook data!
             print(message)
+            orderbook_data = message["data"]
+
+        # Now, we can subscribe to the orderbook stream and pass our arguments:
+        # our function and our selected symbol.
+        # To subscribe to multiple symbols, pass a list: ["BTCUSD", "ETHUSD"]
+        # To subscribe to all symbols, pass "*".
+        ws_inverse.orderbook_25_stream(handle_orderbook, "BTCUSD")
+
+
+        # To subscribe to private data, the process is the same:
         def handle_position(message):
+            # I will be called every time there is new position data!
             print(message)
 
-        ws.orderbook_25_stream(handle_orderbook, "BTCUSD")
-        ws.position_stream(handle_position)
+        ws_inverse.position_stream(handle_position)
+
+
+        # Similarly, if you want to listen to the WebSockets of other markets:
+        ws_spot = spot.WebSocket(test=True)
+        # handle_orderbook() will now be called for both inverse and spot data.
+        # To keep the data separate, simply create another function and pass it below.
+        ws_spot.depth_v2_stream(handle_orderbook, "BTCUSDT")
+
 
         while True:
-            # Run your main trading strategy here
-            pass  # To avoid CPU utilisation, use time.sleep(1)
+            # This while loop is required for the program to run. You may execute
+            # additional code for your trading logic here.
+            sleep(1)
+        print('----------')
 
-        return Response(status=status.HTTP_200_OK)
 
 
 
