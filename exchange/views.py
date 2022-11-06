280b8be37b96392ac0161da5c4213efcc358189a
Fan Wang <nimadorostkar97@gmail.com>
2022-11-06 10:14:06 +0330

    2

--- exchange/views.py
+++ exchange/views.py
@@ -16,6 +16,8 @@ from time import sleep
 from pybit import inverse_perpetual
 from pybit import spot
 
+import ccxt
+
 
 
 #------------------------------------------------- SourceExchanges -------------
@@ -77,58 +79,32 @@ class ExchangeData(APIView):
         api_key=exchange.api_key
         api_secret=exchange.api_secret
 
-        """
-        An alternative way to import:
-        from pybit.inverse_perpetual import WebSocket, HTTP
-        """
+        session = inverse_perpetual.HTTP(endpoint='https://api.bybit.com', api_key=api_key, api_secret=api_secret)
+        #orderbook = session.orderbook(symbol='BTCUSD')
+        #info_symbol = session.latest_information_for_symbol(symbol='BTCUSD')
+        #query_symbol = session.query_symbol()
+        #wallet_balance = session.get_wallet_balance(coin="BTC")
+        #active_order = session.get_active_order(symbol='BTCUSD')
+        #my_position = session.my_position()
+        #api_key_info = session.api_key_info()
 
-        # Set up logging (optional)
-        import logging
-        logging.basicConfig(filename="pybit.log", level=logging.DEBUG,format="%(asctime)s %(levelname)s %(message)s")
+        bybit = ccxt.bybit({ "apiKey":api_key, "secret":api_secret })
 
+        order = bybit.futuresPrivatePostOrderCreate({
+            'symbol':"BTCUSD",
+            'side' : side,
+            'order_type' : 'Limit',
+            'qty' : 2,
+            'time_in_force': "GoodTillCancel",
+            'price' : price
+        })
 
-        # Connect with authentication!
-        ws_inverse = inverse_perpetual.WebSocket(
-            test=True,
-            api_key=api_key,  # omit the api_key & secret to connect w/o authentication
-            api_secret=api_secret,
-            # to pass a custom domain in case of connectivity problems, you can use:
-            domain="bytick"  # the default is "bybit"
-        )
-
-        # Let's fetch the orderbook for BTCUSD. First, we'll define a function.
-        def handle_orderbook(message):
-            # I will be called every time there is new orderbook data!
-            print(message)
-            orderbook_data = message["data"]
-
-        # Now, we can subscribe to the orderbook stream and pass our arguments:
-        # our function and our selected symbol.
-        # To subscribe to multiple symbols, pass a list: ["BTCUSD", "ETHUSD"]
-        # To subscribe to all symbols, pass "*".
-        ws_inverse.orderbook_25_stream(handle_orderbook, "BTCUSD")
+        return Response(bybit, status=status.HTTP_200_OK)
 
 
-        # To subscribe to private data, the process is the same:
-        def handle_position(message):
-            # I will be called every time there is new position data!
-            print(message)
-
-        ws_inverse.position_stream(handle_position)
 
 
-        # Similarly, if you want to listen to the WebSockets of other markets:
-        ws_spot = spot.WebSocket(test=True)
-        # handle_orderbook() will now be called for both inverse and spot data.
-        # To keep the data separate, simply create another function and pass it below.
-        ws_spot.depth_v2_stream(handle_orderbook, "BTCUSDT")
 
-
-        while True:
-            # This while loop is required for the program to run. You may execute
-            # additional code for your trading logic here.
-            sleep(1)
-        print('----------')
 
 
 
