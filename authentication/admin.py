246c7c2f0c382b07c3cea76061ed039c39241ed6
Fan Wang <nimadorostkar97@gmail.com>
2022-11-12 09:14:42 +0330

    custom_usermanager

--- authentication/admin.py
+++ authentication/admin.py
@@ -4,7 +4,7 @@ from .models import User
 
 #-------------------------------------------------------------------------------
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('img', 'username', 'email', 'date_joined')
+    list_display = ('img', 'email', 'date_joined')
     list_filter = ('date_joined',)
-    search_fields = ['email', 'firs_name', 'last_name', 'username']
+    search_fields = ['email', 'firs_name', 'last_name']
 admin.site.register(User, UserAdmin)
--- authentication/models.py
+++ authentication/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from authentication.custom_usermanager import UserManager
 from django.utils.html import format_html
+
+
 
 #-------------------------------------------------------------------------------
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=256, unique=True)
     photo = models.ImageField(upload_to='user/photo', default='user/photo/default.png', null=True, blank=True)
     phone = models.CharField(max_length=256, null=True, blank=True)
     bio = models.TextField(max_length=1000, null=True, blank=True)
+    otp = models.PositiveIntegerField(blank=True, null=True)
+    otp_create_time = models.DateTimeField(auto_now=True)
+    confirmed = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     def __str__(self):
         return str(self.username)
 
     def img(self):
         return format_html("<img style='width:30px;border-radius:50%;' src='{}'>".format(self.photo.url))
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
