26714ade5d3839644789914e2fae2ec2f88d5c61
Fan Wang <nimadorostkar97@gmail.com>
2022-11-15 18:29:58 +0330

    l

--- exchange/consumers.py
+++ exchange/consumers.py
@@ -12,23 +12,13 @@ class ExchangeConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
-    async def receive(self, text_data=None, bytes_data=None, **kwargs):
-
-        print(text_data)
-        await self.send("zzzzzzz")
-
-
-        exchange = Exchange.objects.get(id=1)
-        api_key=exchange.api_key
-        api_secret=exchange.api_secret
-
         # Connect with authentication!
         ws_inverse = inverse_perpetual.WebSocket(
             test=True,
-            api_key=api_key,  # omit the api_key & secret to connect w/o authentication
-            api_secret=api_secret,
+            api_key="62402a681634ba000102877f",  # omit the api_key & secret to connect w/o authentication
+            api_secret="bb2ce4a8-ff4a-4859-965d-547e0fa43068",
             # to pass a custom domain in case of connectivity problems, you can use:
-            domain="bytick"  # the default is "bybit"
+            domain="bybit"  # the default is "bybit"
         )
 
         # Let's fetch the orderbook for BTCUSD. First, we'll define a function.
@@ -51,13 +41,11 @@ class ExchangeConsumer(AsyncJsonWebsocketConsumer):
 
         ws_inverse.position_stream(handle_position)
 
-
         # Similarly, if you want to listen to the WebSockets of other markets:
         ws_spot = spot.WebSocket(test=True)
         # handle_orderbook() will now be called for both inverse and spot data.
         # To keep the data separate, simply create another function and pass it below.
         ws_spot.depth_v2_stream(handle_orderbook, "BTCUSDT")
-
 
         while True:
             # This while loop is required for the program to run. You may execute
@@ -69,18 +57,15 @@ class ExchangeConsumer(AsyncJsonWebsocketConsumer):
 
 
 
-
-
-
-'''
-class ExchangeConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
         if text_data == 'PING':
             await self.send('PONG')
 
+
+    async def send_markets(self, **kwargs):
+        message = event['text']
+        await self.send(message)
+
+
     async def disconnect(self, close_code):
         self.event_close = True
-'''
